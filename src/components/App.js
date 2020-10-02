d49a88f1e6aeb2d9466698b705583baab183034d
Byko <bykocrypto@gmail.com>
2020-10-02 09:08:23 +0200

    🛎️ Dapp  foundation & basic dependencies

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import './App.css'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import uniswapLogo from '../uniswap-logo.png'
+import daiLogo from '../dai-logo.png'
+
+
+function App() {
+  
+    }
+  
+  return (
+    <div>
+      
+    </div>
+  );
+
+
+export default App
